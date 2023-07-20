942e8c99aa1f223188596f517ecd2dc6ee86a9c9
Abhay Raj <abhayaraj06@gmail.com>
2023-07-19 22:46:13 -0700

    css thicker border

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,7 +9,7 @@ export default function Card(props: any) {
   );
 
   return (
-    <a href="#" className="group transition ease-in-out mb-7 hover:shadow-lg hover:shadow-indigo-600 hover:border-indigo-500 block p-6 border rounded-lg shadow bg-slate-900 border-gray-700">
+    <a href="#" className="border-2 group transition ease-in-out mb-7 hover:shadow-lg hover:shadow-indigo-600 hover:border-indigo-500 block p-6 rounded-lg shadow bg-slate-900 border-gray-700">
       <div className="flex items-center justify-between">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">{props.service_name}</h5>
         <div className="h-8 mb-2 p-2 border border-green-400 rounded-lg items-center justify-center text-green-400 inline-flex">
