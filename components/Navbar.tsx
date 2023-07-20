85ddd86cd8fc0c520f12a70c81ad36f5b1d0b680
Abhay Raj <abhayaraj06@gmail.com>
2023-07-19 22:42:58 -0700

    css changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar(props: any) {
         <nav className="border-gray-200 bg-gray-900">
           <div className="flex flex-wrap items-center justify-between mx-10 p-4">
             <Link href="/" className="flex items-center">
-              <span id = "logo-text" className="text-transparent bg-clip-text bg-gradient-to-r to-blue-500 from-sky-400">volapyk</span>
+              <span id = "logo-text" className="text-transparent bg-clip-text bg-gradient-to-r to-purple-600 from-indigo-600">volapyk</span>
             </Link>
             <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
@@ -15,19 +15,19 @@ export default function Navbar(props: any) {
             <div className="hidden w-full md:block md:w-auto" id="navbar-default">
               <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
-                  <Link href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-500 rounded md:bg-transparent md:text-blue-500 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
+                  <Link href="/" className="block border-2 border-transparent text-purple-600 bg-purple-600 rounded bg-transparent p-2 hover:border-purple-600 hover:bg-purple-600 hover:bg-opacity-20 ease-in-out duration-150" aria-current="page">Home</Link>
                 </li>
                 <li>
-                  <Link href="/about" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</Link>
+                  <Link href="/about" className="block border-2 border-transparent text-white bg-purple-600 rounded bg-transparent p-2 hover:text-purple-600 hover:border-purple-600 hover:bg-purple-600 hover:bg-opacity-20 ease-in-out duration-150">About</Link>
                 </li>
                 <li>
-                  <Link href="/add-service" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Add a Service</Link>
+                  <Link href="/add-service" className="block border-2 border-transparent text-white bg-purple-600 rounded bg-transparent p-2 hover:text-purple-600 hover:border-purple-600 hover:bg-purple-600 hover:bg-opacity-20 ease-in-out duration-150">Add a Service</Link>
                 </li>
                 <li>
-                  <Link href="/instant-checker" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Instant Checker</Link>
+                  <Link href="/instant-checker" className="block border-2 border-transparent text-white bg-purple-600 rounded bg-transparent p-2 hover:text-purple-600 hover:border-purple-600 hover:bg-purple-600 hover:bg-opacity-20 ease-in-out duration-150">Instant Checker</Link>
                 </li>
                 <li>
-                  <Link href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
+                  <Link href="#" className="block border-2 border-transparent text-white bg-purple-600 rounded bg-transparent p-2 hover:text-purple-600 hover:border-purple-600 hover:bg-purple-600 hover:bg-opacity-20 ease-in-out duration-150">Contact</Link>
                 </li>
               </ul>
             </div>
