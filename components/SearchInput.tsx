baa8e0152e9829333fb381c965568481a69fab7f
Abhay Raj <abhayaraj06@gmail.com>
2023-07-21 09:59:47 -0700

    Update SearchInput.tsx

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -29,7 +29,7 @@ const SearchInput = () => {
                     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                     </div>
-                    <input value = {searchQuery} onChange={(event) => setSearchQuery(event.target.value)} type="search" id="default-search" className="transition ease-in-out hover:shadow-lg hover:shadow-indigo-600 block w-full p-4 pl-10 text-sm border rounded-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 bg-slate-900 border-gray-600 placeholder-gray-400 text-white" placeholder="Search for a service here..." required></input>
+                    <input value = {searchQuery} onChange={(event) => setSearchQuery(event.target.value)} type="search" id="default-search" className="transition ease-in-out hover:shadow-lg hover:shadow-indigo-600 block w-full p-4 pl-10 text-sm border-2 rounded-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 bg-slate-900 border-gray-600 placeholder-gray-400 text-white" placeholder="Search for a service here..." required></input>
                     <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-indigo-500 hover:bg-indigo-500 focus:ring-4 focus:outline-none focus:ring-indigo-400 font-medium rounded-lg text-sm px-4 py-2 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus:ring-indigo-800">Search</button>
                 </div>
             </form>
