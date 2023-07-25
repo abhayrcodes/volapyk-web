0723e8d63ada9755f729f0a8e3bdff3d4d3dd331
Abhay Raj <abhayaraj06@gmail.com>
2023-07-24 22:21:04 -0700

    search functionality

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -6,7 +6,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
     try {
         const { searchParams } = new URL(request.url || "");
-        const query = decodeURI(searchParams.get('q'));
+        const query = decodeURI(searchParams.get('q') || "");
         // Open the SQLite database
         const db = await open({
             filename: 'frontend2.db',
