90ce34c51432593cceacba37dc67663ac0763674
Abhay Raj <abhayaraj06@gmail.com>
2023-09-16 18:31:02 -0700

    changed singup page

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -7,119 +7,53 @@ import { useState } from 'react';
 
 export default function SignUp() {
 
-    const [slide, setSlide] = useState(0);
-
-    function handleForward(e: any) { 
-        e.preventDefault();
-        setSlide(slide+1);
-    }
-
-    function handleBack(e: any) {
-        e.preventDefault();
-        setSlide(slide-1)
-    }
-
     return (
         <div>
             <Navbar/>
 
-            <form className="flex h-full">
-            <div className={`h-full border-2 border-indigo-600 shadow-lg shadow-indigo-600 p-10 ${slide==0 ? "w-[91%]" : "w-[3%]"}`}>
-                <div className={`${slide==0 ? "" : "hidden"} h-full`}>
-                    <SignUpTab count={slide} onClick={handleForward}/>
-                </div>
-            </div>
+            <div className="flex w-11/12 mx-auto my-5">
 
-            <div className={`border-2 border-indigo-600 shadow-lg shadow-indigo-600 p-10 ${slide==1 ? "w-[91%]" : "w-[3%]"}`}>
-                <div className={`${slide==1 ? "" : "hidden"}`}>
-                    <SignUpTab count={slide} onClick={handleForward}/>
-                    <button className=" hidden lg:block text-center p-[15px] w-[60px] hover:-translate-x-1/4 font-bold border-2 border-slate-100 bg-slate-100 rounded-lg hover:text-slate-100 hover:bg-slate-900 transition ease-in-out duration-300 hover:shadow-lg hover:shadow-indigo-600">
-                        <span>←</span>
-                    </button>
-                </div>
-            </div>
+                <div className="transition ease-in-out duration-200 block border-indigo-600 border-2 bg-slate-100 lg:rounded-lg md:rounded-md p-10 shadow-lg shadow-indigo-600 text-slate-900 w-7/12">
+                    <div className="flex gap-5 mb-10">
+                        <img src={'/images/logo2.svg'} className="-translate-y-3" height={50} width={50} alt="React Logo" />
+                        <h1 id='logo-text' className="mb-4 text-3xl font-extrabold text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-purple-600 from-indigo-600">volapyk</span></h1>
+                    </div>
 
-            <div className={`border-2 border-indigo-600 shadow-lg shadow-indigo-600 p-10 ${slide==2 ? "w-[91%]" : "w-[3%]"}`}>
-                <div className={`${slide==2 ? "" : "hidden"}`}>
-                    <SignUpTab count={slide} onClick={handleForward}/>
-                </div>
-            </div>
 
-            <div className={`border-2 border-indigo-600 shadow-lg shadow-indigo-600 p-10 ${slide==3 ? "w-[91%]" : "w-[3%]"}`}>
-                <div className={`${slide==3 ? "" : "hidden"}`}>
-                    <SignUpTab count={slide} onClick={handleForward}/>
+                    <div className="text-2xl text-slate-900">
+                        <div className="my-10"><span className="rounded-lg border-2 border-slate-900 px-2 bg-slate-900 text-slate-100">1.</span> Access to new, <span className="animate-text bg-clip-text text-transparent bg-gradient-to-r to-purple-600 from-indigo-600 px-5 py-2 rounded-lg">exclusive</span> features!</div>
+                        <div className="my-10"><span className="rounded-lg border-2 border-slate-900 px-2 bg-slate-900 text-slate-100">2.</span> Be up to date on the <span className="animate-text bg-clip-text text-transparent bg-gradient-to-r to-purple-600 from-indigo-600 px-5 py-2 rounded-lg">latest</span> project changes!</div>
+                        <div className="my-10"><span className="rounded-lg border-2 border-slate-900 px-2 bg-slate-900 text-slate-100">3.</span> Get <span className="animate-text bg-clip-text text-transparent bg-gradient-to-r to-purple-600 from-indigo-600 px-5 py-2 rounded-lg">personalized notifications</span> about the services you care about!</div>
+                    </div>
+
+                    <span className="animate-text text-2xl text-white bg-gradient-to-r to-purple-600 from-indigo-600 px-5 py-2 rounded-lg">And so much more!</span>
                 </div>
+
+                <div className="ml-10 w-6/12">
+                    <div className="flex row-span-1 mb-10">
+
+                            
+                            <h2 className="text-slate-100 lg:text-4xl text-bold">Create a <span className="animate-text bg-clip-text text-transparent bg-gradient-to-r to-purple-600 from-indigo-600 px-5 py-2 rounded-lg">volapyk</span>account today!</h2>
+                    </div>
+
+                    <div>
+                        <form>
+                            <h2 className="labil text-slate-100 lg:text-2xl font-semibold">Email</h2>
+                            <input placeholder = "Email" className="transition ease-in-out hover:shadow-lg hover:shadow-indigo-600 block w-full p-4 pl-5 text-sm border-2 rounded-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 bg-slate-900 border-gray-600 placeholder-gray-400 text-white mb-5 mt-2"></input>
+                            <h2 className="labil text-slate-100 lg:text-2xl font-semibold">Password</h2>
+                            <input placeholder = "Password" className="transition ease-in-out hover:shadow-lg hover:shadow-indigo-600 block w-full p-4 pl-5 text-sm border-2 rounded-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 bg-slate-900 border-gray-600 placeholder-gray-400 text-white mb-5 mt-2"></input>
+                            <input placeholder = "Confirm password" className="transition ease-in-out hover:shadow-lg hover:shadow-indigo-600 block w-full p-4 pl-5 text-sm border-2 rounded-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 bg-slate-900 border-gray-600 placeholder-gray-400 text-white mb-5 mt-2"></input>
+                            <button type="submit" className="lg:rounded-lg transition ease-in-out duration-300 labil font-semibold w-full p-4 bg-slate-100 text-slate-900 my-5 border-2 border-slate-100 hover:text-slate-100 hover:bg-slate-900 hover:shadow-lg hover:shadow-indigo-600">CREATE YOUR ACCOUNT</button>
+                        </form>
+                    </div>
+
+                    <div>
+                        <h4 className="text-slate-100 text-center">Already have an account? <Link href="/?login=true" className="text-indigo-400">Log in</Link></h4>
+                    </div>
+                </div>
+
             </div>
-            </form>
             
         </div>
     )
   }
-  
-const SignUpTab = ({count, onClick} : any) => {
-    return (
-        <div className="h-full">
-        <div className="row-span-1 mb-10">
-
-        <div className=" content-center w-fit grid place-items-center mr-5 animate-bounce">
-            <div className="transition duration-150 ease-in-out h-5 w-5 rounded-full bg-transparent border-2 border-slate-200 mb-1 shadow-lg shadow-indigo-600"></div>
-            <div className="transition duration-150 ease-in-out h-5 w-10 bg-transparent border-2 border-slate-200 rounded-tl-full rounded-tr-full m-0 shadow-lg shadow-indigo-600"></div>
-        </div>
-        <h2 className="text-slate-100 lg:text-4xl" id="logo-text">Sign Up</h2>
-        </div>
-
-        <div>
-        <h2 className="labil text-slate-100 lg:text-2xl font-semibold">Username</h2>
-        <RegularInput placeholder = "Username" background = "d" otheredits = "mb-5 mt-2"></RegularInput>
-        <h2 className="labil text-slate-100 lg:text-2xl font-semibold">Password</h2>
-        <RegularInput placeholder = "Password" background = "d" otheredits="my-2"></RegularInput>
-        <RegularInput placeholder = "Confirm Password" background = "d" otheredits="my-2"></RegularInput>
-        <button onClick={onClick} className="lg:rounded-lg transition ease-in-out duration-300 labil font-semibold w-full p-4 bg-slate-100 text-slate-900 my-5 border-2 border-slate-100 hover:text-slate-100 hover:bg-slate-900 hover:shadow-lg hover:shadow-indigo-600">BEGIN ACCOUNT SETUP</button>
-        </div>
-
-        <div>
-        <h4 className="text-slate-100 text-center">Already have an account? <Link href="/?login=true" className="text-indigo-400">Log in</Link></h4>
-        </div>
-        </div>
-    )
-}
-/*
-<div className="transition ease-in duration-150 border-2 border-indigo-600 shadow-lg shadow-indigo-600 p-10 rounded-lg w-1/2 mx-auto my-5 bg-slate-900">
-                <div className="flex row-span-1 mb-10">
-                        <div className=" content-center w-fit grid place-items-center mr-5 animate-bounce">
-                            <div className="transition duration-150 ease-in-out h-5 w-5 rounded-full bg-transparent border-2 border-slate-200 mb-1 shadow-lg shadow-indigo-600"></div>
-                            <div className="transition duration-150 ease-in-out h-5 w-10 bg-transparent border-2 border-slate-200 rounded-tl-full rounded-tr-full m-0 shadow-lg shadow-indigo-600"></div>
-                        </div>
-                        <h2 className="text-slate-100 lg:text-4xl" id="logo-text">Sign Up</h2>
-                </div>
-
-                <div>
-                    <form>
-                        <h2 className="labil text-slate-100 lg:text-2xl font-semibold">Username</h2>
-                        <RegularInput placeholder = "Username" background = "d" otheredits = "mb-5 mt-2"></RegularInput>
-                        <h2 className="labil text-slate-100 lg:text-2xl font-semibold">Password</h2>
-                        <RegularInput placeholder = "Password" background = "d" otheredits="my-2"></RegularInput>
-                        <RegularInput placeholder = "Confirm Password" background = "d" ></RegularInput>
-                        <button type="submit" className="lg:rounded-lg transition ease-in-out duration-300 labil font-semibold w-full p-4 bg-slate-100 text-slate-900 my-5 border-2 border-slate-100 hover:text-slate-100 hover:bg-slate-900 hover:shadow-lg hover:shadow-indigo-600">CREATE ACCOUNT</button>
-                    </form>
-                </div>
-              </div>
-              */
-
-
-/*
-<form className="flex w-full">
-                <div className="border-2 border-indigo-600 rounded-lg shadow-lg shadow-indigo-600 p-10">
-                    <Link href="#">hi</Link>
-                </div>
-                <div className="border-2 border-indigo-600 rounded-lg shadow-lg shadow-indigo-600 p-10">
-                    <Link href="#">hi</Link>
-                </div>
-                <div className="border-2 border-indigo-600 rounded-lg shadow-lg shadow-indigo-600 p-10">
-                    <Link href="#">hi</Link>
-                </div>
-                <div className="border-2 border-indigo-600 rounded-lg shadow-lg shadow-indigo-600 p-10">
-                    <Link href="#">hi</Link>
-                </div>
-              </form>
-              */
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,21 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        text: 'text 1s ease infinite',
+      },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
     },
   },
   plugins: [],
